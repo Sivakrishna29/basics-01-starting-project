da8eca7e5c07808b8e09c653466e73b87a0fb9d5
Sivakrishna29 <sivakrishnakasaragadda@gmail.com>
2023-01-07 21:40:02 +0530

    Add new repo

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const defaultResult = 0;
+let currenResult = defaultResult;
+currenResult = currenResult + 10;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currenResult, calculationDescription);
